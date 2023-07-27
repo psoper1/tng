78b7d3fde5851e432f6e37931d6106cf7a0db7df
psoper1 <psoper0604@gmail.com>
2023-07-26 21:53:47 -0400

    poo

--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 function Nav() {
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg custom-navbar">
                 <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
