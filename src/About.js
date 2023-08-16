f57f387e8bce9d7737a106415e84791b87c06051
psoper1 <psoper0604@gmail.com>
2023-08-15 20:13:12 -0400

    cleaned up about page

--- src/About.js
+++ src/About.js
@@ -13,8 +13,8 @@ function About({activeLink, setActiveLink}) {
           <div className="col-md-12">
             <h2 className="text-center text-white">About True Nature Gaming</h2>
             <div className="mb-4">
-              <h3 className="text-white"><u>Our Story</u></h3>
-              <p className="text-white">
+              <h3 className="text-white text-center"><u>Our Story</u></h3>
+              <p className="text-white text-center">
                 True Nature Gaming was founded by a passionate group of friends
                 who share an insatiable love for gaming. We spend
                 countless hours competing and bonding,
@@ -27,8 +27,8 @@ function About({activeLink, setActiveLink}) {
             </div>
 
             <div className="mb-4">
-              <h3 className="text-white"><u>Our Mission</u></h3>
-              <p className="text-white">
+              <h3 className="text-white text-center"><u>Our Mission</u></h3>
+              <p className="text-white text-center">
                 Our mission at True Nature Gaming is simple yet profound - to create
                 an inclusive and welcoming environment for gaming
                 enthusiasts. We want to provide a space where gamers of all skill
@@ -36,7 +36,7 @@ function About({activeLink, setActiveLink}) {
                 not just about the competition, but about fostering a sense of
                 community, sportsmanship, and celebration of the gaming spirit.
               </p>
-              <p className="text-white">
+              <p className="text-white text-center">
                 At True Nature Gaming, we believe that gaming is not just a hobby but
                 a way of life. We are driven by our genuine love for gaming
                 and the incredible connections it enables.
@@ -44,27 +44,27 @@ function About({activeLink, setActiveLink}) {
             </div>
 
             <div className="mb-4">
-              <h3 className="text-white"><u>What Sets Us Apart</u></h3>
-              <p className="text-white">
+              <h3 className="text-white text-center"><u>What Sets Us Apart</u></h3>
+              <p className="text-white text-center">
                 At True Nature Gaming, we stand out from the crowd in several ways:
               </p>
               <ul>
-                <li className="text-white">
+                <li className="listitem text-white">
                   <strong>Player-Centric Approach:</strong> Our tournaments are designed
                   with players in mind, ensuring fair play, exciting challenges, and
                   engaging experiences for everyone involved.
                 </li>
-                <li className="text-white">
+                <li className="listitem text-white">
                   <strong>Community Building:</strong> We believe in building a close-knit
                   community where players can interact, form teams, and make new friends
                   with shared interests.
                 </li>
-                <li className="text-white">
+                <li className="listitem text-white">
                   <strong>Passionate Organizers:</strong> Our team of organizers and
                   moderators are avid gamers themselves, and they are dedicated to
                   delivering well-organized and enjoyable tournaments.
                 </li>
-                <li className="text-white">
+                <li className="listitem text-white">
                   <strong>Prizes and Rewards:</strong> Get ready for some fantastic rewards!
                   True Nature Gaming hosts tournaments with attractive prizes and recognition
                   for top-performing teams and players.
@@ -73,8 +73,8 @@ function About({activeLink, setActiveLink}) {
             </div>
 
             <div>
-              <h3 className="text-white"><u>Join Us</u></h3>
-              <p className="text-white">
+              <h3 className="text-white text-center"><u>Join Us</u></h3>
+              <p className="text-white text-center">
                 We welcome players of all gaming backgrounds to be a part of the True Nature
                 Gaming community. Whether you are a seasoned vet or a newcomer
                 eager to dive into the fun, there's a place for you here. <NavLink to="/signup" onClick={() => setActiveLink("signup")}>Join our upcoming
